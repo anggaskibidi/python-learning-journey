a316555e9ea9b37b006f01c3ac8ce4557ea97f11
AnggaSkibidi <anggasubekti996@gmail.com>
2026-05-06 23:44:24 +0700

    perubahan

--- materi.py
+++ materi.py
@@ -1,2 +1,8 @@
-nama = 'angga skibidi'
-print (nama)
+nama = 'angga skibidi' #tipe string karena menggunakan tanda petik satu atau petik dua
+usia = 15 #tipe integer karena tidak menggunakan tanda petik dan merupakan bilangan bulat
+tinggi_badan = 170.5 #tipe float karena tidak menggunakan tanda petik dan merupakan bilangan desimal
+
+sapa = 'halo dunia, my name is: '
+print (sapa + nama)
+print (usia)
+print (tinggi_badan)
